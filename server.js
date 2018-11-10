9ddc9f149a8509ff9ed62fc9d97da3812aa6400b
BenjaminDagg <bdagg@csumb.edu>
2018-11-09 23:10:03 -0800

    added server for deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  httpServer = http.Server(app);
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/src/index.html');
+});
+app.listen(process.env.PORT || 3000);
