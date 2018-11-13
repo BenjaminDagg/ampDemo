34013adaef3fac76515240ee84eafaeb32078e74
BenjaminDagg <bdagg@csumb.edu>
2018-11-12 20:03:43 -0800

    fixed bug in mobile version where sidebar idnt close when clicking a nav option

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/src/index.html');
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
